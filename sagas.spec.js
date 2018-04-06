5b388fba5a25482bb8e3cb384a0e0494920fd94d
Monilito Castro <mannycastro.epic@gmail.com>
2018-04-05 21:18:29 -0500

    included tests for the two other sagas, brought in 'put' saga effect

--- sagas.spec.js
+++ sagas.spec.js
@@ -3,18 +3,27 @@ import test from "tape";
 import { incrementAsync } from "./sagas";
 
 import { delay } from "redux-saga";
-import { call } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
 
 test("incremental Saga test", assert => {
   const gen = incrementAsync();
 
   assert.deepEqual(
-    gen.next(),
-    {
-      done: false,
-      value: call(delay, 1000)
-    },
+    gen.next().value,
+    call(delay, 1000),
     "increment Async should return a Promise that will resolve after 1 second"
+  );
+
+  assert.deepEqual(
+    gen.next().value,
+    put({ type: "INCREMENT" }),
+    "incrementAsync Saga must dispatch an INCREMENT action"
+  );
+
+  assert.deepEqual(
+    gen.next(),
+    { done: true, value: undefined },
+    "incrementAsync Saga must be done"
   );
 
   assert.end();
